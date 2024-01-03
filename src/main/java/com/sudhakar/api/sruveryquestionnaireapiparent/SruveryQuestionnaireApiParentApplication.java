f107853134664ce36c0a18adb1a611b002796540
gusudhakar <sudhakarguguloth1999@gmail.com>
2024-01-03 15:06:54 +0530

    first commit

---
+++ src/main/java/com/sudhakar/api/sruveryquestionnaireapiparent/SruveryQuestionnaireApiParentApplication.java
@@ -0,0 +1,13 @@
+package com.sudhakar.api.sruveryquestionnaireapiparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SruveryQuestionnaireApiParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SruveryQuestionnaireApiParentApplication.class, args);
+	}
+
+}
