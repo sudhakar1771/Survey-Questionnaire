5d81eace8ec1bc1975644f197c563d776395176d
gusudhakar <sudhakarguguloth1999@gmail.com>
2024-01-04 12:07:17 +0530

    usage of @PathVariable

--- src/main/java/com/sudhakar/api/sruveryquestionnaireapiparent/helloworld/HelloWorld.java
+++ src/main/java/com/sudhakar/api/sruveryquestionnaireapiparent/helloworld/HelloWorld.java
@@ -1,11 +1,15 @@
 package com.sudhakar.api.sruveryquestionnaireapiparent.helloworld;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
+//@Controller
 public class HelloWorld {
 
     @RequestMapping("/helloworld")
@@ -14,9 +18,11 @@ public class HelloWorld {
         return "my first api";
     }
 
-    @RequestMapping("/helloworldbean")
-    public HelloWorldBean returnbean(){
-        return new HelloWorldBean("hi i am bean");
+    @RequestMapping("/helloworldbean/{name}/{message}")
+    public HelloWorldBean returnbean(@PathVariable Map<String,String> multipleparams){
+        String name = multipleparams.get("name");
+        String message = multipleparams.get("message");
+        return new HelloWorldBean("hello world " + name + " " +message);
     }
 
 
