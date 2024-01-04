0e021f0e1fa858a6b6fd33d9bf16959efc917d70
gusudhakar <sudhakarguguloth1999@gmail.com>
2024-01-04 11:42:48 +0530

    first sample API

---
+++ src/main/java/com/sudhakar/api/sruveryquestionnaireapiparent/helloworld/HelloWorld.java
@@ -0,0 +1,24 @@
+package com.sudhakar.api.sruveryquestionnaireapiparent.helloworld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @RequestMapping("/helloworld")
+    //@ResponseBody
+    public String returnhelloworld(){
+        return "my first api";
+    }
+
+    @RequestMapping("/helloworldbean")
+    public HelloWorldBean returnbean(){
+        return new HelloWorldBean("hi i am bean");
+    }
+
+
+
+}
---
+++ src/main/java/com/sudhakar/api/sruveryquestionnaireapiparent/helloworld/HelloWorldBean.java
@@ -0,0 +1,15 @@
+package com.sudhakar.api.sruveryquestionnaireapiparent.helloworld;
+
+public class HelloWorldBean {
+
+    private String message;
+
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
